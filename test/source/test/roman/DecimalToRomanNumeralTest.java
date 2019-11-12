83def60dcc710763fded1f0fe47d3a70232c3436
Nicola Meneghini <nicolameneghini@hotmail.it>
2019-11-12 23:24:09 +0100

    roman exercise completed

--- test/source/test/roman/DecimalToRomanNumeralTest.java
+++ test/source/test/roman/DecimalToRomanNumeralTest.java
@@ -25,19 +25,43 @@ public class DecimalToRomanNumeralTest {
         RomanNumeral romanNumeral = new RomanNumeral(i);
         assertEquals(s, romanNumeral.toString());
     }
-
-
-    @Test
-    void twoisII(){
-        check(2, "II");
-    }
-
-
     @Test
     void oneisI(){
         check(1, "I");
     }
 
     @Test
+    void twoisII(){
+        check(2, "II");
+    }
+
+    @Test
     void threeisIII(){ check(3, "III");}
+
+    @Test
+    void fourisIV(){check(4, "IV");}
+
+    @Test
+    void sevenidVII(){check(7, "VII");}
+
+    @Test
+    void fourteenisXIV(){check(14, "XIV");}
+
+    @Test
+    void twentyfourisXXIV(){check(24, "XXIV");}
+
+    @Test
+    void eightyfourisLXXXIV(){check(84, "LXXXIV");}
+
+    @Test
+    void ninetynineisXCVII(){check(99, "XCIX");}
+
+    @Test
+    void twohundredtwentyfourisXXIV(){check(224, "CCXXIV");}
+
+    @Test
+    void eighthundredeightyfourisLXXXIV(){check(884, "DCCCLXXXIV");}
+
+    @Test
+    void ninehundredninetynineisXCVII(){check(999, "CMXCIX");}
 }
